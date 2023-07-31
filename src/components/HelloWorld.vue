b2e35689c8f143cda04cc0593482e763a186d128
yeding <kanaumachi@gmail.com>
2023-07-31 16:50:37 +0900

    add naiveui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { NButton } from 'naive-ui'
 defineProps({
   msg: {
     type: String,
@@ -15,6 +16,7 @@ defineProps({
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
+    <n-button>naive-ui</n-button>
   </div>
 </template>
 
