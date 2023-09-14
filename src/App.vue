e11302c1bda2764331eef74c80a3609ef390553d
yeding <kanaumachi@gmail.com>
2023-09-14 18:37:43 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <n-layout position="absolute">
       <n-layout-header>
         <n-space justify="center" :align="'center'" :size="50">
-          <n-button  @click="handleKeyUp" size="large">导入文本</n-button>
+          <n-button @click="handleKeyUp" size="large">导入文本</n-button>
           <n-button size="large">导出文本</n-button>
-          <n-switch size="large">
+          <n-switch size="large" v-model:value="isTranslate">
             <template #checked>
               翻译
             </template>
@@ -15,24 +15,45 @@
           </n-switch>
         </n-space>
       </n-layout-header>
-      <n-layout-content style="padding-top: 5%;width: 70%;margin: auto" >
-        <n-dynamic-input  @keyup="handleKeyUp"  v-model:value="lyricList" placeholder="请输入" :min="1"  />
+      <n-layout-content style="padding-top: 5%;width: 70%;margin: auto">
+        <n-dynamic-input v-if="!isTranslate" @keyup="handleKeyUp" v-model:value="lyricList" placeholder="请输入" :min="1" />
       </n-layout-content>
-      
     </n-layout>
   </n-space>
 </template>
 
 <script setup>
 
-
 const lyricList = ref([""])
 
-function handleKeyUp(value,index) {
-  // if(value.code === 'Enter') {
-  //   lyricList.value.push("");
-  // }
+async function handleKeyUp(value) {
+  if (value.code === 'Enter') {
+    if (findRootNodeWithElement(value.target, 'data-key') === (lyricList.value.length - 1).toString()) {
+      lyricList.value.push("");
+      await nextTick();
+    }
+    let inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+      // 如果是最后一个，则焦点回到第一个
+      if (value.srcElement == inputs[i]) {
+        inputs[i + 1].focus(); break;
+      }
+    }
+  }
 }
+function findRootNodeWithElement(element, targetTagName) {
+  // 从当前元素开始向上遍历DOM树
+  if (element !== null && element.getAttribute(targetTagName)) {
+    return element.getAttribute(targetTagName);
+  } else if (element.parentElement) {
+    // 如果找到包含目标元素的根节点，则返回该根节点；否则返回null
+    return findRootNodeWithElement(element.parentElement, targetTagName);
+  }
+  return null
+}
+
+const isTranslate = ref(false)
+
 </script>
 
 <style scoped>
