acb4b9056cda3cd2a3b6b8f3a2e2725192a29841
yeding <kanaumachi@gmail.com>
2023-11-14 18:11:36 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: "docs"
+  },
   plugins: [
     vue(),
     AutoImport({
